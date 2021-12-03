0841800fdaadf2494d406ad5d0af4190e74fc379
s25866-pj <92215147+s25866-pj@users.noreply.github.com>
2021-12-03 16:56:48 +0100

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 10; i++) {
+        cout << endl;
+        for (int j = 0; j < 10; j++) {
+            switch (j) {
+            case 0 :
+                cout << i<<" ";
+                break;
+            case 1:
+                cout << i + i<<" ";
+                break;
+            case 2:
+                cout << i * i <<" ";
+                break;
+            case 3:
+                cout << i + j << " ";
+                break;
+            case 4:
+                cout << i - j << " ";
+            default:
+                cout << "0";
+           }
+        }
+    }
+}
