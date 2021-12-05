24c474642122406734e12b1d6493a04edeb5531a
s25866-pj <92215147+s25866-pj@users.noreply.github.com>
2021-12-05 22:21:04 +0100

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cout << "podaj rozmiar tablicy"<<endl;
+    cin >> size;
+    int h_size = size / 2;
+    int* tab=new int [size];
+    for (int i = 0; i < size; i++) {
+        cin >> tab[i];
+    }
+    for (int i = 0; i < h_size; i++) {
+        cout << endl;
+        for (int j = 0; j < 2; j++) {
+            if (j % 2 == 0) {
+                cout << tab[i];
+             }
+            else {
+                cout <<"   |    "<<tab[h_size + i];
+            }
+
+        }
+
+    }
+    cout << endl << "zamiana"<<endl;
+    for (int i = 0; i < h_size; i++) {
+        swap(tab[i], tab[h_size + i]);
+    }
+    cout << "tablica po zamianie:" << endl;
+    for (int i = 0; i < h_size; i++) {
+        cout << endl;
+        for (int j = 0; j < 2; j++) {
+            if (j % 2 == 0) {
+                cout << tab[i];
+            }
+            else {
+                cout << "   |    " << tab[h_size + i];
+            }
+
+        }
+
+    }
+    return 0;
+}
