81a9f05a035e83ca62a3c0b7e17bbbbcc7b67e70
s25866-pj <92215147+s25866-pj@users.noreply.github.com>
2021-12-18 12:43:16 +0100

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+int main()
+{
+    int n,number=0;
+    std::cout << "Podaj swoja liczbe:" << std::endl;
+    std:: cin >> n;
+    for (int i = n - 1; i > 0; i--) {
+        if (i % 5 == 0 && i % 3 != 0) {
+            number++;
+            std::cout << i << std::endl;
+        }
+    }
+    std::cout<<std::endl;
+    std::cout << "liczb podzielnych przez 5 i nie podzielnych przez 3 mniejszych od twojej liczby jest:" << number;
+
+}
